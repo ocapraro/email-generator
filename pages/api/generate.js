271c0d81da57ac0e88ae293ffb841d34329bec45
Oscar Capraro <oscar@mcapraro.com>
2022-04-22 09:13:40 -0700

    fix: make max tokens comply with openai guidlines

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,7 +9,7 @@ export default async function (req, res) {
   const completion = await openai.createCompletion("text-davinci-002", {
     prompt: generatePrompt(req.body.recipients, req.body.senders, req.body.questions,req.body.tags),
     temperature: 0.6,
-    max_tokens:256
+    max_tokens:250
   });
   res.status(200).json({ result: completion.data.choices[0].text.replace(/^(\n)+/,"") });
 }
